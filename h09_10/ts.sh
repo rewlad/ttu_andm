d43ad0b3c7fc88303120c672691676abf90bf4e3
Sergei Kirjanov <rewlad@gmail.com>
2012-12-17 18:21:38 +0200

    apriori

--- h09_10/ts.sh
+++ h09_10/ts.sh
@@ -1,5 +1,5 @@
-
+#!/bin/bash
 perl ts.pl < tshekid_office2003.csv > input2.txt
 ./apriori -ts input2.txt output-s.txt
-./apriori -tr -s3 input2.txt output-r-s3.txt
+./apriori -tr -s3.5 input2.txt output-r-s3.txt
 ./apriori -tr -c60 input2.txt output-r-c60.txt
