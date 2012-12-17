04da1fa91baa2fe5b4026674dc38b62d77c547bc
Sergei Kirjanov <rewlad@gmail.com>
2012-12-17 12:31:06 +0200

    apriori

---
+++ h09_10/ts.pl
@@ -0,0 +1,13 @@
+use strict;
+use utf8;
+my $prev_c;
+binmode STDIN,":utf8";
+binmode STDOUT,":utf8";
+for(<>){
+    /^(\d+),"([^"]+)",,/ or print STDERR "undef:$_" and next;
+    my($cur_c,$txt)=($1,$2);
+    $txt=~s/\W/_/g;
+    print $cur_c eq $prev_c ? " ": "\n";
+    $prev_c = $cur_c;
+    print $txt;
+}
